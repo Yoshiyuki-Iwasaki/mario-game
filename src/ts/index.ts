deda8d5bf3ce36c8e4b6de04bbe8ce86dda9e07a
Yoshiyuki Iwasaki <armada3524@gmail.com>
2021-11-04 12:17:08 +0900

    create game.

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,13 +1,34 @@
-import { tabFunction } from "./module/tab";
-import { pageTopBtn } from "./module/pageTop";
-import { modalFunction } from "./module/modal";
-import { hamburgerFunction } from "./module/hamburger";
-import { smoothScrollFunction } from "./module/smoothScroll";
-import { slickFunction } from "./module/slick";
+/**
+ * イベント名の列挙型
+ */
+const enum EventName {
+  LOAD = "load",
+  CLICK = "click",
+  MOUSE_MOVE = "mousemove",
+}
 
-slickFunction();
-tabFunction();
-pageTopBtn();
-modalFunction();
-hamburgerFunction();
-smoothScrollFunction();
+const character: any = document.getElementById("character");
+const block: any = document.getElementById("block");
+
+window.addEventListener(EventName.CLICK, () => jump());
+
+const jump = () => {
+  if (!character?.classList.contains("animate")) {
+    character?.classList.add("animate");
+  }
+  setTimeout(() => {
+    character?.classList.remove('animate');
+  },500)
+}
+
+const checkDead = setInterval(() => {
+  const characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+  const blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert('you lose.');
+  }
+},10)
